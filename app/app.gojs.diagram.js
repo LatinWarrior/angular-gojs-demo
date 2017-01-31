c9de260e8359d66340d399acde9bc3f99942c9df
Luis Blanco <GZKKLY@NAM.corp.gm.com>
2017-01-31 14:26:36 -0600

    Added angular-bootstrap

--- app/app.gojs.diagram.js
+++ app/app.gojs.diagram.js
@@ -6,38 +6,30 @@
 
 		let myCrap = this;		
 
-		console.log('myCrap: ', myCrap);		
+		// console.log('myCrap: ', myCrap);		
 
-		console.log('Inside of templateUrl. $element: ', $element);
+		// console.log('Inside of templateUrl. $element: ', $element);
 
-		console.log('Inside of templateUrl. $element[0]: ', $element[0]);
+		// console.log('Inside of templateUrl. $element[0]: ', $element[0]);
 
-		console.log('Inside of templateUrl begin');
+		// console.log('Inside of templateUrl begin');
 
 		let diagram = $element[0].querySelector('#myDiagram');
 
-		console.log('diagram: ', diagram);
+		// console.log('diagram: ', diagram);
 
-		console.log('Inside of temaplateUrl end');
+		// console.log('Inside of temaplateUrl end');
 
 		return 'app/app.gojs.diagram.html' 
 	}
 
 	function controller($scope, go, $$) {
-		console.log('In gojs Diagram');
-		console.log('$scope: ', $scope);
-		let ctrl = this;
-		// console.log('In gojs diagram ctrl: ', ctrl);
-		//ctrl.go = go;
-		// ctrl.name = 'Peter Griffin';
-		// console.log('ctrl Width: ', ctrl.width);
-		// console.log('scope Width: ', $scope.width);
-		// console.log('Diagram Name: ', ctrl.diagramName);
-		// console.log('go: ', go);
-		// console.log('$$', $$);
-		console.log('$scope.$parent.$ctrl.model', $scope.$parent.$ctrl.model);
-		console.log('ctrl', ctrl);
-		console.log('$scope.id', $scope.$parent.$ctrl.id);
+		// console.log('In gojs Diagram');
+		// console.log('$scope: ', $scope);
+		let ctrl = this;		
+		// console.log('$scope.$parent.$ctrl.model', $scope.$parent.$ctrl.model);
+		// console.log('ctrl', ctrl);
+		// console.log('$scope.id', $scope.$parent.$ctrl.id);
 
 		let parent = $scope.$parent.$ctrl;		
 		
@@ -58,12 +50,53 @@
 			"undoManager.isEnabled": true
 		});
 
+		// define a Node template
+		diagram.nodeTemplate =
+		  $$(go.Node, "Horizontal",
+		    // the entire node will have an orange background
+		    { background: "#DD4814" },
+		    $$(go.Picture,
+		      // Pictures are best off having an explicit width and height.
+		      // Adding a red background ensures something is visible when there is no source set
+		      { margin: 10, width: 50, height: 50, background: "red" },
+		      // Picture.source is data bound to the "source" attribute of the model data
+		      new go.Binding("source")),
+		    $$(go.TextBlock,
+		      "Default Text",  // the initial value for TextBlock.text
+		      // some room around the text, a larger font, and a white stroke:
+		      { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
+		      // TextBlock.text is data bound to the "name" attribute of the model data
+		      new go.Binding("text", "name"))
+		  );
+
 		var myModel = $$(go.Model);
 
 		myModel.nodeDataArray = ctrl.model;
 		
 		diagram.model = myModel;
 
+	}
+
+	// whenever a GoJS transaction has finished modifying the model, update all Angular bindings
+	function updateAngular(e) {
+		if (e.isTransactionFinished) {
+		 	scope.$apply();
+		}
+	}
+
+	// update the Angular model when the Diagram.selection changes
+	function updateSelection(e) {
+		diagram.model.selectedNodeData = null;
+		var it = diagram.selection.iterator;
+		while (it.next()) {
+			  var selnode = it.value;
+			  // ignore a selected link or a deleted node
+			  if (selnode instanceof go.Node && selnode.data !== null) {
+				    diagram.model.selectedNodeData = selnode.data;
+				    break;
+			  }
+		}
+		scope.$apply();
 	}
 
 	module.component('gojsDiagram', {			
--- app/app.main.js
+++ app/app.main.js
@@ -15,9 +15,9 @@
 			let ctrl = this;
 			let _$ = go.GraphObject.make;			
 			ctrl.model = [
-				{ key: 'Alpha'},
-				{ key: 'Beta'},
-				{ key: 'Gamma'}
+				{ name: 'Alpha', source: '/app/images/cat1.png'},
+				{ name: 'Beta', source: '/app/images/cat2.png'},
+				{ name: 'Gamma', source: '/app/images/cat3.png'}
 			];
 			ctrl.name = 'familyGuyDiagram';
 			ctrl.id = 'familyGuyDiagramId';
@@ -25,9 +25,9 @@
 			ctrl.height = '175';
 			ctrl.bcolor = '#fff000';
 			ctrl.style = 'background-color: "' + ctrl.bcolor + '"; width: "' + ctrl.width + '"; height: "' + ctrl.height + '";';
-			console.log('In appMain component begin');
-			console.log('ctrl: ', ctrl);
-			console.log('In appMain component end');
+			// console.log('In appMain component begin');
+			// console.log('ctrl: ', ctrl);
+			// console.log('In appMain component end');
 		}
 
 }(angular.module('myApp')));
