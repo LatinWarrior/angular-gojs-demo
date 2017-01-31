f04e1db14b47c812043882faa0015193fef01e5b
Luis Blanco <GZKKLY@NAM.corp.gm.com>
2017-01-31 15:43:53 -0600

    Added more functionality

--- app/app.gojs.diagram.js
+++ app/app.gojs.diagram.js
@@ -24,14 +24,9 @@
 	}
 
 	function controller($scope, go, $$) {
-		// console.log('In gojs Diagram');
-		// console.log('$scope: ', $scope);
-		let ctrl = this;		
-		// console.log('$scope.$parent.$ctrl.model', $scope.$parent.$ctrl.model);
-		// console.log('ctrl', ctrl);
-		// console.log('$scope.id', $scope.$parent.$ctrl.id);
-
-		let parent = $scope.$parent.$ctrl;		
+		
+		let ctrl = this,
+			parent = $scope.$parent.$ctrl;		
 		
 		ctrl.name = parent.name || 'myDiagramName';
 		//ctrl.id = parent.id || 'myDiagramId';
@@ -42,10 +37,11 @@
 		ctrl.model = parent.model || [{ key: 'No model provided' }];
 
 		// Create a Diagram for the given HTML DIV element
-
 		var diagram = $$(go.Diagram, "myDiagramId", {
 			// Center diagram contents.
 			initialContentAlignment: go.Spot.Center,
+			"ModelChanged": updateAngular,
+            "ChangedSelection": updateSelection,
 			// Enable Ctrl-Z to undo and Ctrl-Y to redo.
 			"undoManager.isEnabled": true
 		});
@@ -69,6 +65,49 @@
 		      new go.Binding("text", "name"))
 		  );
 
+		// define a Link template that routes orthogonally, with no arrowhead
+        diagram.linkTemplate =
+            $$(go.Link,
+                { routing: go.Link.Orthogonal, corner: 5 },
+                $$(go.Shape, { strokeWidth: 3, stroke: "#555" })
+                ); // the link shape
+
+        // Set up a Part as a legend, and place it directly on the diagram
+    diagram.add(
+      $$(go.Part, "Table",
+        { position: new go.Point(500, 10), selectable: false },
+        $$(go.TextBlock, "Key",
+          { row: 0, font: "700 14px Arial, Helvetica, sans-serif" }),  // end row 0
+        $$(go.Panel, "Horizontal",
+          { row: 1, alignment: go.Spot.Left },
+          $$(go.Shape, "Rectangle",
+            { desiredSize: new go.Size(20, 20), fill: '#CC293D', margin: 5 }),
+          $$(go.TextBlock, "Always Execute",
+            { font: "700 13px Arial, Helvetica, sans-serif" })
+        ),  // end row 1
+        $$(go.Panel, "Horizontal",
+          { row: 2, alignment: go.Spot.Left },
+          $$(go.Shape, "Rectangle",
+            { desiredSize: new go.Size(20, 20), fill: '#FFD700', margin: 5 }),
+          $$(go.TextBlock, "Left Only",
+            { font: "700 13px Arial, Helvetica, sans-serif" })
+        ),  // end row 2
+        $$(go.Panel, "Horizontal",
+          { row: 3, alignment: go.Spot.Left },
+          $$(go.Shape, "Rectangle",
+            { desiredSize: new go.Size(20, 20), fill: '#009CCC', margin: 5 }),
+          $$(go.TextBlock, "Right Only",
+            { font: "700 13px Arial, Helvetica, sans-serif" })
+        ),  // end row 3
+        $$(go.Panel, "Horizontal",
+          { row: 4, alignment: go.Spot.Left },
+          $$(go.Shape, "Rectangle",
+            { desiredSize: new go.Size(20, 20), fill: '#F09AAA', margin: 5 }),
+          $$(go.TextBlock, "Both",
+            { font: "700 13px Arial, Helvetica, sans-serif" })
+        )  // end row 4
+      ));
+
 		let myModel = $$(go.Model);
 
 		myModel.nodeDataArray = ctrl.model;
@@ -86,20 +125,24 @@
 	      	{ text: "lightblue", color: "#33D3E5" }
 	    ]);
 
+	    document.getElementById("displayModel").value = diagram.model.toJson();
+
 	    // Whenever a GoJS transaction has finished modifying the model, update all Angular bindings
-		function updateAngular(e) {
-			if (e.isTransactionFinished) {
+		function updateAngular(e) {			
+			if (e.isTransactionFinished) {				
+				// console.log('In updateAngular');
 			 	$scope.$apply();
 			}
 		}
 
 		// Update the Angular model when the Diagram.selection changes
 		function updateSelection(e) {
+			// console.log('In updateSelection');
 			diagram.model.selectedNodeData = null;
 			var it = diagram.selection.iterator;
 			while (it.next()) {
 				  var selnode = it.value;
-				  // ignore a selected link or a deleted node
+				  // Ignore a selected link or a deleted node
 				  if (selnode instanceof go.Node && selnode.data !== null) {
 					    diagram.model.selectedNodeData = selnode.data;
 					    break;
